8f7195edd64fd171ec0d58e95bc0fd4812f8d2a4
eXiga <anton.kostenich@hotmail.com>
2019-09-14 17:34:14 +0200

    implement basic ui for login page

---
+++ client/lib/home.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Welcome")),
+      body: Center(
+        child: Container (
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(36.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text("LOL")
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ client/lib/login.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:byomug/widgets/card.dart';
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+           Container(
+            child: Padding(
+              padding: EdgeInsets.only(left: 28.0, right: 28.0, top: 60.0),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Text("BYOM",
+                          style: TextStyle(
+                                  fontFamily: "AT",
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold
+                                )
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 160,
+                  ),
+                  FormCard(),
+                  SizedBox(
+                    height: 30,
+                  ),
+                   InkWell(
+                    child: Container(
+                      width: double.infinity,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(colors: [
+                            Colors.greenAccent,
+                            Colors.green
+                          ]),
+                          borderRadius: BorderRadius.circular(6.0),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.green.withOpacity(.3),
+                                offset: Offset(0.0, 8.0),
+                                blurRadius: 8.0)
+                          ]),
+                      child: Material(
+                        color: Colors.transparent,
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.pushReplacementNamed(context, '/home');
+                          },
+                          child: Center(
+                            child: Text("Sign in",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: "AT",
+                                    fontSize: 22,
+                                    letterSpacing: 1.0)),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ]
+              )
+            )
+          ),
+        ],
+      ),
+    );
+  }
+}
